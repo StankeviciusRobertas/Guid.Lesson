90fd1b250ae4d66aeef30f5d41da6de243e2527f
Robertas Stankevičius <stankevic.robertas@gmail.com>
2023-10-25 18:37:05 +0300

    Add project files.

---
+++ Guid.Lesson/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace GuidLesson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Guid guid = Guid.NewGuid();
+            Console.WriteLine(guid);
+
+            var cardGuid = Guid.Parse(Console.ReadLine());
+        }
+    }
+}
